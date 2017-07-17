f0d890b561758fb513ff1fd259a1ac743959183a
Ryan S <kobitoko@gmail.com>
2017-07-17 17:12:22 -0600

    Initial Commit 2

---
+++ csSpriteSheetPreviewExporter/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace csSpriteSheetPreviewExporter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.previewImageBox = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.GifButton = new System.Windows.Forms.Button();
+            this.buttonZoomOut = new System.Windows.Forms.Button();
+            this.buttonZoomIn = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.previewImageBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OpenFile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // previewImageBox
+            // 
+            this.previewImageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.previewImageBox.Location = new System.Drawing.Point(3, 16);
+            this.previewImageBox.Name = "previewImageBox";
+            this.previewImageBox.Size = new System.Drawing.Size(580, 410);
+            this.previewImageBox.TabIndex = 2;
+            this.previewImageBox.TabStop = false;
+            this.previewImageBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.previewImageBox_MouseDown);
+            this.previewImageBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.previewImageBox_MouseMove);
+            this.previewImageBox.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.previewImageBox_MouseWheel);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox5);
+            this.groupBox1.Controls.Add(this.GifButton);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 41);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Size = new System.Drawing.Size(168, 429);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // GifButton
+            // 
+            this.GifButton.Location = new System.Drawing.Point(12, 398);
+            this.GifButton.Name = "GifButton";
+            this.GifButton.Size = new System.Drawing.Size(107, 23);
+            this.GifButton.TabIndex = 0;
+            this.GifButton.Text = "Export Gif";
+            this.GifButton.UseVisualStyleBackColor = true;
+            this.GifButton.Click += new System.EventHandler(this.GifButton_Click);
+            // 
+            // buttonZoomOut
+            // 
+            this.buttonZoomOut.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonZoomOut.Location = new System.Drawing.Point(657, 13);
+            this.buttonZoomOut.Name = "buttonZoomOut";
+            this.buttonZoomOut.Size = new System.Drawing.Size(18, 23);
+            this.buttonZoomOut.TabIndex = 1;
+            this.buttonZoomOut.Text = "-";
+            this.buttonZoomOut.UseVisualStyleBackColor = true;
+            this.buttonZoomOut.Click += new System.EventHandler(this.buttonZoomOut_Click);
+            // 
+            // buttonZoomIn
+            // 
+            this.buttonZoomIn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonZoomIn.Location = new System.Drawing.Point(729, 13);
+            this.buttonZoomIn.Name = "buttonZoomIn";
+            this.buttonZoomIn.Size = new System.Drawing.Size(19, 23);
+            this.buttonZoomIn.TabIndex = 0;
+            this.buttonZoomIn.Text = "+";
+            this.buttonZoomIn.UseVisualStyleBackColor = true;
+            this.buttonZoomIn.Click += new System.EventHandler(this.buttonZoomIn_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.previewImageBox);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(168, 41);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(586, 429);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(754, 41);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Controls.Add(this.buttonZoomIn);
+            this.groupBox4.Controls.Add(this.buttonZoomOut);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox4.Location = new System.Drawing.Point(0, 470);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(754, 40);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(681, 14);
+            this.textBox1.MaxLength = 5;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(42, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "100%";
+            this.textBox1.WordWrap = false;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Location = new System.Drawing.Point(0, 6);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(168, 75);
+            this.groupBox5.TabIndex = 1;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "groupBox5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 510);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.previewImageBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox previewImageBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button buttonZoomOut;
+        private System.Windows.Forms.Button buttonZoomIn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button GifButton;
+        private System.Windows.Forms.GroupBox groupBox5;
+    }
+}
+
---
+++ csSpriteSheetPreviewExporter/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using AnimatedGif;
+using System.Threading.Tasks;
+
+namespace csSpriteSheetPreviewExporter
+{
+    public partial class Form1 : Form
+    {
+        List<Bitmap> bitmapList = new List<Bitmap>();
+        List<string> imageStringList = new List<string>();
+        int indexImg = 0;
+        int fps = 33; // 1000/30 == 30fps
+        float zoom = 1;
+        Timer t = new Timer();
+        int[] position = { 0, 0 };
+        int[] lastMousePosition = { -1, -1 };
+        bool shouldRender = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // https://docs.microsoft.com/en-us/dotnet/framework/winforms/advanced/working-with-images-bitmaps-icons-and-metafiles
+            // https://msdn.microsoft.com/en-us/library/system.drawing.graphics.aspx
+
+            // Add the paint event to the picture box
+            previewImageBox.Paint += new PaintEventHandler(this.previewImageBox_Paint);
+            groupBox5.Enabled = false;
+            groupBox5.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Multiselect = true;
+			// filter examples: https://msdn.microsoft.com/en-us/library/system.windows.forms.filedialog.filter(v=vs.110).aspx
+            file.Filter = "Image files (*.png)|*.png|All files (*.*)|*.*";
+            //string directoryPath = Path.GetDirectoryName(filePath);
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                indexImg = 0;
+                shouldRender = false;
+                t.Stop();
+                t.Dispose();
+                t = new Timer();
+                imageStringList.Clear();
+                foreach(Image i in bitmapList)
+                {
+                    i.Dispose();
+                }
+                bitmapList.Clear();
+                imageStringList.AddRange(file.FileNames.ToList());
+                if (imageStringList.Count == 1) {
+                    groupBox5.Enabled = true;
+                    groupBox5.Visible = true;
+                }
+                foreach (string s in imageStringList)
+                {
+                    bitmapList.Add(new Bitmap(s));
+                }
+                previewImageBox.Refresh();
+                t.Interval = fps;
+                t.Start();
+                t.Tick += new EventHandler(renderUpdate);
+                shouldRender = true;
+            }
+        }
+
+        private void renderUpdate(object sender, EventArgs e)
+        {
+            previewImageBox.Refresh();
+            indexImg = (indexImg + 1) % bitmapList.Count;
+        }
+
+        // https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.control.paint?view=netframework-4.5
+        private void previewImageBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (!shouldRender)
+                return;
+            Graphics g = e.Graphics;
+            if (bitmapList.Count > 0)
+            {
+                Image img = bitmapList[indexImg];
+                float ratio = img.Width / img.Height;
+                float newHeight = img.Height * zoom;
+                g.DrawImage(img, position[0], position[1], newHeight*ratio, newHeight);
+            }
+        }
+
+        private void previewImageBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                lastMousePosition[0] = e.X;
+                lastMousePosition[1] = e.Y;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                position[0] = 0;
+                position[1] = 0;
+            }
+        }
+
+        private void previewImageBox_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta >= 0)
+                zoomIn(true);
+            else if (e.Delta < 0)
+                zoomIn(false);
+        }
+
+        private void previewImageBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+            // Move sprite image preview
+            position[0] += e.X - lastMousePosition[0];
+            position[1] += e.Y - lastMousePosition[1];
+
+            lastMousePosition[0] = e.X;
+            lastMousePosition[1] = e.Y;
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonZoomIn_Click(object sender, EventArgs e)
+        {
+            zoomIn(true);
+        }
+
+        private void buttonZoomOut_Click(object sender, EventArgs e)
+        {
+            zoomIn(false);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            changeTextZoom();
+        }
+
+        private void textBox1_TextChanged(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                changeTextZoom();
+        }
+
+        private void zoomIn(bool makeBigger)
+        {
+            int zoomIn = (makeBigger ? 1 : -1);
+            zoom += zoomIn * 0.05f;
+            updateZoom();
+        }
+
+        private void changeTextZoom()
+        {
+            char[] seperators = {' ', '%'};
+            string input = textBox1.Text.Split(seperators)[0];
+            int result = 0;
+            if (!int.TryParse(input, out result))
+                return;
+            zoom = result / 100f;
+            updateZoom();
+        }
+
+        private void updateZoom()
+        {
+            if (zoom > 15f)
+                zoom = 15f;
+            if (zoom < 0.01f)
+                zoom = 0.01f;
+            textBox1.Text = string.Format("{0:0}%", zoom * 100);
+        }
+
+        private async void GifButton_Click(object sender, EventArgs e)
+        {
+            if (imageStringList.Count == 0)
+            {
+                MessageBox.Show("No file was loaded, cannot export an empty gif", "Nothing to export", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            GifButton.Enabled = false;
+            // Taken from https://stackoverflow.com/questions/14962969/how-can-i-use-async-to-increase-winforms-performance
+            Task task = Task.Run(() => exportToGif());
+            await task;
+            GifButton.Enabled = true;
+        }
+        
+        private void exportToGif()
+        {
+            // Taken from https://github.com/mrousavy/AnimatedGif
+            // Remove file extension, and preserves the path of the imported file(s).
+            string filename = Path.GetFileNameWithoutExtension(imageStringList[0]);
+            string path = Path.GetDirectoryName(imageStringList[0]);
+			// Exporting the gif in the same directory as source images. Should ask for confirmation
+            using (AnimatedGifCreator gifCreator = AnimatedGif.AnimatedGif.Create($"{path}\\Animated_{filename}.gif", fps))
+            {
+                //Enumerate through a List<System.Drawing.Image> or List<System.Drawing.Bitmap> for example
+                foreach (string fullPath in imageStringList)
+                {
+                    //Add the image to gifEncoder with default Quality
+                    gifCreator.AddFrame(fullPath, GifQuality.Bit8);
+                }
+            } // gifCreator.Finish and gifCreator.Dispose is called here
+            MessageBox.Show("Exporting gif finished");
+        }
+        
+    }
+}
+;
